2991f8ebac5226a15f7e987345ba7afacefb6f5e
Agustin Hahn <agustin.f.hahn@gmail.com>
2023-09-21 19:39:31 -0300

    primer subida

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import EnlaceInicial from './components/enlaceInicial'
+
+function App() {
+
+  return (
+    <>
+      <div className='containerApp'>
+        <h1 className='tituloh1'>TURNOS GO</h1>
+        <EnlaceInicial />
+        <EnlaceInicial />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/EnlaceInicial.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EnlaceInicial = () => {
+
+    let textoBoton = "texto en boton"
+
+    return (
+        <div>
+            <button style={{margin:'30px'}}>
+                {textoBoton}
+            </button>
+        </div>
+    )
+}
+
+export default EnlaceInicial
